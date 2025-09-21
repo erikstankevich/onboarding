6e060ce4addd985b74eff2965f89f57584f8b715
ees <21estankevich@gmail.com>
2025-09-21 11:25:48 +0200

    typo file name

--- 5_darkmode/ligthmode.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 1
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 1

ee4e4560dcbe292ede36a5231785bc53722fbea3
ees <21estankevich@gmail.com>
2025-09-21 11:20:41 +0200

    in case not all was staged

---
+++ 4_wallpaper/openimage.ps1
@@ -0,0 +1,3 @@
+Start-Process "C:\repos\onboarding\4_wallpaper\_material\90278.jpg"
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "RotatingLockScreenEnabled" -Value 0
